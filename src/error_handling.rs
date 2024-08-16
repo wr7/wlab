4e39822fd6980d1fa853d5fac730467f23b699da
wr7 <d-wr7@outlook.com>
2024-08-16 15:43:08 -0500

    Improve multi-snippet diagnostic rendering
    Now, two snippets within 3 lines of eachother will be merged together.
    
    Additionally, the gutters of all snippets within the same diagnostic will be
    aligned

--- src/error_handling.rs
+++ src/error_handling.rs
@@ -6,7 +6,7 @@ use std::{borrow::Cow, ops::Range};
 
 use wutil::Span;
 
-use crate::util;
+use crate::util::{self, MaxVec};
 
 /// Includes information about where something appears in a source file
 #[derive(Debug, Clone, Copy)]
@@ -59,28 +59,194 @@ macro_rules! diagnostic {
 
 impl Diagnostic {
     pub fn render(&self, code: &str) -> String {
-        let mut ret_val = "\n ".to_owned();
+        let mut renderer = DiagnosticRenderer::new(code, &self.hints, &self.msg);
 
-        ret_val += &self.msg;
-        ret_val.push('\n');
-
-        for _ in 0..ret_val.len() {
-            ret_val.push('-');
+        for hint in &self.hints {
+            renderer.render_hint(hint);
         }
 
-        ret_val.push('\n');
+        renderer.finish()
+    }
+}
 
-        for (i, hint) in self.hints.iter().enumerate() {
-            ret_val += &hint.render_snippet(code);
+#[derive(Clone, Copy)]
+struct ResolvedHintRef<'a> {
+    hint: &'a Hint,
+    lines: Span,
+    columns: Span,
+}
 
-            if i != self.hints.len() - 1 {
-                ret_val += "\n ...\n";
+impl<'a> Deref for ResolvedHintRef<'a> {
+    type Target = &'a Hint;
+
+    fn deref(&self) -> &Self::Target {
+        &self.hint
+    }
+}
+
+struct DiagnosticRenderer<'a> {
+    code: &'a str,
+    output: String,
+    padding: u32,
+    backlog: MaxVec<ResolvedHintRef<'a>, 2>,
+}
+
+impl<'a> DiagnosticRenderer<'a> {
+    pub fn new(code: &'a str, hints: &[Hint], diagnostic_msg: &str) -> Self {
+        let mut output = "\n ".to_owned();
+
+        output += diagnostic_msg;
+        output.push('\n');
+
+        for _ in 0..output.len() {
+            output.push('-');
+        }
+
+        output.push('\n');
+
+        let mut max_byte_index = None;
+        let mut min_byte_index = None;
+
+        for hint in hints {
+            max_byte_index = Some(max_byte_index.unwrap_or(hint.span.end).max(hint.span.end));
+            min_byte_index = Some(min_byte_index.unwrap_or(hint.span.end).min(hint.span.end));
+        }
+
+        let padding =
+            if let Some((max_byte_index, min_byte_index)) = max_byte_index.zip(min_byte_index) {
+                let (line_end, _) =
+                    util::line_and_col(code, max_byte_index.saturating_sub(1).max(min_byte_index));
+                line_end.ilog10()
+            } else {
+                0
+            };
+
+        Self {
+            code,
+            padding,
+            output,
+            backlog: MaxVec::new(),
+        }
+    }
+
+    fn render_hint(&mut self, hint: &'a Hint) {
+        let next_hint = hint.resolve(self.code);
+        let prev_hint = self.backlog.cycle(next_hint);
+        let hint = self.backlog[0];
+
+        if self.backlog.len() <= 1 {
+            return;
+        }
+
+        self.direct_render_hint(prev_hint, hint, Some(next_hint));
+    }
+
+    fn direct_render_hint(
+        &mut self,
+        prev_hint: Option<ResolvedHintRef>,
+        hint: ResolvedHintRef,
+        _next_hint: Option<ResolvedHintRef>,
+    ) {
+        let mut leading_lines_to_render = 2;
+
+        if let Some(prev_hint) = prev_hint {
+            for _ in 0..=self.padding {
+                self.output.push(' ');
+            }
+
+            if hint.lines.start.wrapping_sub(prev_hint.lines.end) <= leading_lines_to_render + 1 {
+                // "join" together two code snippets //
+                leading_lines_to_render =
+                    (hint.lines.start - prev_hint.lines.end).saturating_sub(1);
+
+                self.output += " |\n";
+            } else {
+                self.output += "...\n";
             }
         }
 
-        ret_val.push('\n');
+        for (i, line) in self
+            .code
+            .lines()
+            .enumerate()
+            .map(|(i, l)| (i + 1, l))
+            .take(hint.lines.end)
+            .skip(hint.lines.start.saturating_sub(leading_lines_to_render + 1))
+        {
+            // Print line number, line, and gutter //
+            let padding = self.padding - i.ilog10();
 
-        ret_val
+            for _ in 0..padding {
+                self.output += " ";
+            }
+
+            self.output += &i.to_string();
+            self.output += " | ";
+            self.output += line;
+            self.output += "\n";
+
+            if !(hint.lines.start..=hint.lines.end).contains(&i) || line.is_empty() {
+                continue;
+            }
+
+            // Print pointer gutter //
+
+            for _ in 0..=self.padding {
+                self.output += " ";
+            }
+
+            self.output += " | ";
+
+            // Print pointer
+
+            let arrow_st = if i == hint.lines.start {
+                hint.columns.start
+            } else {
+                1
+            };
+            let arrow_end = if i == hint.lines.end {
+                hint.columns.end
+            } else {
+                line.chars().count()
+            };
+
+            for _ in 0..arrow_st - 1 {
+                self.output += " ";
+            }
+
+            for _ in arrow_st..=arrow_end.max(arrow_st) {
+                self.output.push(hint.pointer_char);
+            }
+
+            self.output += "\n";
+        }
+
+        if !hint.msg.is_empty() {
+            // Print message //
+            for _ in 0..=self.padding {
+                self.output += " ";
+            }
+            self.output += " | ";
+            self.output += &hint.msg;
+        }
+
+        self.output.push('\n');
+    }
+
+    pub fn finish(mut self) -> String {
+        match &*self.backlog {
+            [] => {}
+            &[hint] => {
+                self.direct_render_hint(None, hint, None);
+            }
+            &[prev_hint, hint] => {
+                self.direct_render_hint(Some(prev_hint), hint, None);
+            }
+            _ => unreachable!(),
+        }
+
+        self.output.push('\n');
+        self.output
     }
 }
 
@@ -105,74 +271,17 @@ impl Hint {
             pointer_char: '-',
         }
     }
-    fn render_snippet(&self, code: &str) -> String {
+
+    fn resolve<'a>(&'a self, code: &str) -> ResolvedHintRef<'a> {
         let (line_st, col_st) = util::line_and_col(code, self.span.start);
         let (line_end, col_end) =
             util::line_and_col(code, self.span.end.saturating_sub(1).max(self.span.start));
 
-        let mut ret_val = String::new();
-
-        for (i, line) in code
-            .lines()
-            .enumerate()
-            .map(|(i, l)| (i + 1, l))
-            .take(line_end)
-            .skip(line_st.saturating_sub(3))
-        {
-            // Print line number, line, and gutter //
-            let padding = line_end.ilog10() - i.ilog10();
-
-            for _ in 0..padding {
-                ret_val += " ";
-            }
-
-            ret_val += &i.to_string();
-            ret_val += " | ";
-            ret_val += line;
-            ret_val += "\n";
-
-            if !(line_st..=line_end).contains(&i) || line.is_empty() {
-                continue;
-            }
-
-            // Print pointer gutter //
-
-            for _ in 0..=line_end.ilog10() {
-                ret_val += " ";
-            }
-
-            ret_val += " | ";
-
-            // Print pointer
-
-            let arrow_st = if i == line_st { col_st } else { 1 };
-            let arrow_end = if i == line_end {
-                col_end
-            } else {
-                line.chars().count()
-            };
-
-            for _ in 0..arrow_st - 1 {
-                ret_val += " ";
-            }
-
-            for _ in arrow_st..=arrow_end.max(arrow_st) {
-                ret_val.push(self.pointer_char);
-            }
-
-            ret_val += "\n";
+        ResolvedHintRef {
+            hint: self,
+            lines: (line_st..line_end).into(),
+            columns: (col_st..col_end).into(),
         }
-
-        if !self.msg.is_empty() {
-            // Print message //
-            for _ in 0..=line_end.ilog10() {
-                ret_val += " ";
-            }
-            ret_val += " | ";
-            ret_val += &self.msg;
-        }
-
-        ret_val
     }
 }
 
--- src/util.rs
+++ src/util.rs
@@ -1,15 +1,75 @@
+use core::slice;
 use std::{
     borrow::Borrow,
     cell::UnsafeCell,
     collections::HashMap,
     fmt::Debug,
     hash::Hash,
-    mem,
+    mem::{self, MaybeUninit},
     ops::{Deref, Range},
 };
 
 mod intersperse;
 pub use intersperse::Intersperse;
+
+/// A stack-allocated resizable array with a max size of `N`.
+pub(crate) struct MaxVec<V, const N: usize> {
+    storage: [MaybeUninit<V>; N],
+    len: usize,
+}
+
+impl<V, const N: usize> MaxVec<V, N> {
+    pub fn new() -> Self {
+        Self {
+            storage: [const { MaybeUninit::uninit() }; N],
+            len: 0,
+        }
+    }
+
+    /// Pushes to the end. If the `MaxVec` is full, an element is popped from the front and returned.
+    pub fn cycle(&mut self, elem: V) -> Option<V> {
+        if N == 0 {
+            panic!()
+        }
+
+        let ret_val = if self.len >= N {
+            unsafe {
+                let val = Some(self.storage[0].assume_init_read());
+                self.len = N - 1;
+
+                core::ptr::copy(
+                    self.storage.as_ptr().cast::<V>().offset(1),
+                    self.storage.as_mut_ptr().cast::<V>(),
+                    self.len,
+                );
+
+                val
+            }
+        } else {
+            None
+        };
+
+        self.storage[self.len] = MaybeUninit::new(elem);
+        self.len += 1;
+
+        ret_val
+    }
+}
+
+impl<V, const N: usize> AsRef<[V]> for MaxVec<V, N> {
+    fn as_ref(&self) -> &[V] {
+        let ptr = self.storage.as_ptr().cast::<V>();
+        unsafe { slice::from_raw_parts(ptr, self.len) }
+    }
+}
+
+impl<V, const N: usize> Deref for MaxVec<V, N> {
+    type Target = [V];
+
+    fn deref(&self) -> &Self::Target {
+        self.as_ref()
+    }
+}
 
 /// A dynamically-sized array. If only one element is present, it is stored directly, and there is no indirection
 #[derive(Clone, PartialEq, Eq)]
